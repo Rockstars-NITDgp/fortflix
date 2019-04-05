b42325decf478ef2f543aecdaf4b6b1009eeccf9
Avinash-B <boppudiavinash@gmail.com>
2019-04-05 20:07:44 +0530

    cleaning repo

--- fortflix/apiurls.py
+++ fortflix/apiurls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.models import CustomUser
+from rest_framework import routers, serializers, viewsets
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('fortflix.urls')),
-    path('api-auth/', include('fortflix.apiurls')),
+    path('', views.api_main),
 ]
